db085686d2159030c55554186487f54c39aaa5c1
48476 <484764968@qq.com>
2024-10-28 14:37:55 +0800

    feat:(实现了将抛出的异常直接扔给钉钉群机器人 API 进行通知)

--- src/main/java/com/gjy/config/web/GlobalException.java
+++ src/main/java/com/gjy/config/web/GlobalException.java
@@ -4,10 +4,17 @@ package com.gjy.config.web;
 
 import com.gjy.exception.BusinessException;
 import com.gjy.model.Result;
+import com.gjy.utils.DingTalkSendMsgUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+import javax.servlet.ServletRequest;
+import java.nio.charset.Charset;
+import java.util.Objects;
 
 
 @Slf4j
@@ -20,18 +27,18 @@ public class GlobalException {
         return Result.error(e.getCode(), e.getMessage());
     }
 
-//    @ExceptionHandler(value = Exception.class)
-//    public Result<?> exceptionHandler(Exception e, ServletRequest request){
-//        if (Objects.nonNull(request) && request instanceof ContentCachingRequestWrapper) {
-//            ContentCachingRequestWrapper wrapper = (ContentCachingRequestWrapper) request;
-//            log.info("未知异常 request ip: {}, url: {}, body: {}", wrapper.getRemoteAddr(),
-//                    wrapper.getRequestURI(), StringUtils.toEncodedString(wrapper.getContentAsByteArray(),
-//                            Charset.forName(wrapper.getCharacterEncoding())));
-//        }
-//        log.error("未知异常", e);
-//        DingTalkSendMsgUtil.sendToDingTalk(e);
-//        return Result.error("系统异常，请联系管理员");
-//    }
+    @ExceptionHandler(value = Exception.class)
+    public Result<?> exceptionHandler(Exception e, ServletRequest request){
+        if (Objects.nonNull(request) && request instanceof ContentCachingRequestWrapper) {
+            ContentCachingRequestWrapper wrapper = (ContentCachingRequestWrapper) request;
+            log.info("未知异常 request ip: {}, url: {}, body: {}", wrapper.getRemoteAddr(),
+                    wrapper.getRequestURI(), StringUtils.toEncodedString(wrapper.getContentAsByteArray(),
+                            Charset.forName(wrapper.getCharacterEncoding())));
+        }
+        log.error("未知异常", e);
+        DingTalkSendMsgUtil.sendToDingTalk(e);
+        return Result.error("系统异常，请联系管理员");
+    }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public Result<?> exceptionHandler(MethodArgumentNotValidException e) {
