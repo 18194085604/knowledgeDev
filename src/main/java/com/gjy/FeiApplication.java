0618f898ca223db61147d726d27b20f8a47c33ee
48476 <484764968@qq.com>
2024-10-28 11:28:19 +0800

    feat:(初始化项目)

---
+++ src/main/java/com/gjy/FeiApplication.java
@@ -0,0 +1,12 @@
+package com.gjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeiApplication.class, args);
+    }
+
+}
